f92be73b914c79b0fccf9ea18eee24ff64fc783d
Eric Zheng <erryzheng@gmail.com>
2025-05-23 01:10:48 -0400

    4

---
+++ 42_trapping_rain_water.py
@@ -0,0 +1,43 @@
+from typing import List
+
+class Solution:
+    def trap1(self, height: List[int]) -> int:
+        res = 0
+        n = len(height)
+        for mid in range(1, n - 1):
+            lMax, rMax = 0, 0
+            # Find the tallest to the right
+            for j in range(mid, n):
+                rMax = max(rMax, height[j])
+            # Find the tallest to the left
+            for j in range(mid, -1, -1):
+                lMax = max(lMax, height[j])
+            volume = min(lMax, rMax,) - height[mid]
+            res += volume
+        return res
+    
+    def trap2(self, height: List[int]) -> int:
+        n = len(height)
+        if n <= 2:
+            return 0
+        res = 0
+
+        lMax, rMax = [0] * n, [0] * n
+        lMax[0] = height[0]
+        rMax[n - 1] = height[n - 1]
+
+        for i in range(1, n):
+            print("i: ", i)
+            print("lMax[i]: ", lMax[i])
+            print("lMax[i - 1]: ", lMax[i - 1])
+            lMax[i] = max(height[i], lMax[i - 1])
+        print("lMax: ", lMax)
+        for i in range(n - 2, -1, -1):
+            rMax[i] = max(rMax[i], rMax[i + 1])
+        print("rMax: ", rMax)
+        for i in range(1, n - 1):
+            res += min(lMax[i], rMax[i]) - height[i]
+        return res
+    
+sol = Solution()
+print(sol.trap2([0,1,0,2,1,0,1,3,2,1,2,1]))
