68a08377853b8e698df3cbd2a954f67e4b8a247d
Eric Zheng <erryzheng@gmail.com>
2025-05-26 10:40:16 -0400

    5/26

--- 16_3sum_closest.py
+++ 16_3sum_closest.py
@@ -1,17 +1,46 @@
-class Solution:
-    def twoSumClosest(self, nums, start, target):
-        lo = start
-        hi = len(nums) - 1
-        delta = float("inf") # target - sum_
-        while lo < hi:
-            sum_ = nums[lo] + nums[hi]
-            if  abs(delta) > abs(sum_ - target):
-                delta = target - sum_
-            if sum_ < target:
-                lo += 1
-            elif sum_ > target:
-                hi -= 1 # hi is decreasing!!!!!!!!!!
-        return target - delta
+"""
+LeetCode 16: 3Sum Closest
+This is a two-pointer problem.
 
-sol = Solution()
-print(sol.twoSumClosest([-1, 2, 1, -4], 0, 1))
+The goal is to find the closest sum of three numbers to the target.
+"""
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        delta = float('inf')
+        for i in range(len(nums) - 2):
+            sum_ = nums[i] + self.twoSumClosest(i + 1, nums, target - nums[i])
+            if abs(delta) > abs(target - sum_):
+                delta = target - sum_
+            # This could optimize the algorith,
+            # If delta is 0, that means we've found the target already.
+            # There's no need to continue the loop.
+            # So we can break the loop and return immediately.
+            if delta == 0:
+                break
+        return target - delta
+    
+    def twoSumClosest(self, start, nums, target):
+        l, r = start, len(nums) - 1
+        delta = float('inf')
+        while l < r:
+            sum_ = nums[l] + nums[r]
+            if abs(delta) > abs(target - sum_):
+                delta = target - sum_
+            # This could optimize the algorithm,
+            # If delta is 0, that means we've found the target already.
+            # There's no need to continue the loop.
+            # So we can break the loop and return immediately.
+            if delta == 0:
+                break
+            if sum_ > target:
+                r -= 1
+            # The reason why we want to continue 
+            elif sum_ < target:
+                l += 1
+        return target - delta
