67b1554d673b623e2edb51e97d7f5b07d165579c
Eric Zheng <erryzheng@gmail.com>
2025-09-24 12:30:52 -0400

    1094

--- 1094_car_pooling.py
+++ 1094_car_pooling.py
@@ -1,17 +1,13 @@
-from typing import List
-def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        timeline = []
-        for numPassenger, from, to in trips:class Solution:
+class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        timeline = []
-        for numPassenger, start, end in trips:
-            timeline.append((start, numPassenger))
-            timeline.append((end, -numPassenger))
-        timeline.sort(key=lambda x: (x[0], x[1]))
-        curr = 0
-        for time, delta in timeline:
-            curr += delta
-            if curr > capacity:
+        schedule = []
+        for numPeople, from_, to_ in trips:
+            schedule.append((from_, numPeople))
+            schedule.append((to_, -numPeople))
+        schedule.sort(key=lambda x: (x[0], x[1]))
+        currPassenger = 0
+        for _, delta in schedule:
+            currPassenger += delta
+            if currPassenger > capacity:
                 return False
-
         return True
