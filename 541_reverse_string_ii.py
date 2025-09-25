433482f03ca102b4f250b3717543bc22095aec89
Eric Zheng <erryzheng@gmail.com>
2025-09-25 01:18:06 -0400

    541

---
+++ 541_reverse_string_ii.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        letters = list(s)
+        for i in range(0, len(letters), 2*k):
+            letters[i:i+k] = reversed(letters[i:i+k])
+        return "".join(letters)
--- zz.py
+++ zz.py
@@ -1,17 +1,5 @@
-intervals = [[1,2], [1,2], [3,8], [4,5], [6,7]]
-
-events = []
-for s, e in intervals:
-    events.append((s, 1))   # start
-    events.append((e, -1))  # end
-
-# Sort by time, and if same time: process end (-1) before start (+1)
-events.sort(key=lambda x: (x[0], x[1]))
-
-curr = 0
-max_overlap = 0
-for _, delta in events:
-    curr += delta
-    max_overlap = max(max_overlap, curr)
-
-print(max_overlap)  # Output: 3
+s = "0123456789"
+letters = list(s)
+letters[0:3] = reversed(letters[0:3])
+s = "".join(letters)
+print(letters)
