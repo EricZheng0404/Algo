e3226efb22c14dfb50b3b1d530e596d948f77a40
Eric Zheng <erryzheng@gmail.com>
2025-10-02 16:52:03 -0400

    366

---
+++ 366_find_leaves_of_binary_tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        self.res = []
+        self.traversal(root)
+        return self.res
+    
+    def traversal(self, root):
+        if root is None:
+            return 0
+        left = self.traversal(root.left)
+        right = self.traversal(root.right)
+        height = max(left, right) + 1
+        # if this is a new height that's not added to the list
+        if len(self.res) < height:
+            self.res.append([])
+        # We add the value to its height index
+        self.res[height - 1].append(root.val)
+        return height
+
